aacb977dea2a0afc56cfabd17931a8a424347932
YordanPetkov <yordan_petkov180@abv.bg>
2019-04-27 15:01:09 +0300

    Use switch.

--- codeFlow/index.php
+++ codeFlow/index.php
@@ -24,6 +24,33 @@ echo "<br />";
 
 $arr = array ("one" => 1, "two" => 2);
 foreach ($arr as $key => $value){
-    echo $key . " => " . $value;
+    echo $key . " => " . $value . "<br />";
 }
 echo "<br />";
+
+
+$a = 5;
+$b = 6;
+
+if ($a > $b)
+    echo "A is greater than B";
+elseif ($a == $b)
+    echo "A is equal to B";
+else 
+    echo "B is greater than A";
+
+echo "<br />";
+
+
+$a = 1;
+switch ($a) {
+    case 0: 
+        echo "A is 0";
+        break;
+    case 1:
+        echo "A is 1";
+        break;
+    default:
+        echo "A is ... sth different";
+        break;
+}
