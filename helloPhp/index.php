0ee1a975ef47a5a1a97cf89331292418a736e938
YordanPetkov <yordan_petkov180@abv.bg>
2019-04-27 11:45:47 +0300

    Made helloWorld demo.

---
+++ helloPhp/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hello php world</title>
+
+    </head>
+    <body>
+        Standart HTML content followed by php.
+        <br />
+        <?php print ("Hello world!"); ?>
+    </body>
+
+</html>
