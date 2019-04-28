c4d7066efa41e9c91543893602bd5091d50fbf77
YordanPetkov <yordan_petkov180@abv.bg>
2019-04-28 23:37:14 +0300

    changed autoload demo

--- OOP/autoload.php
+++ OOP/autoload.php
@@ -10,5 +10,16 @@ function __autoload($class_name) {
     }
 }
 
+/*function My_autoload($class_name) {
+    $name = "includes/".$class_name.".inc.php";
+    if(file_exists($name)){
+        include $name;
+    }
+    else {
+        echo "Class not found";
+    }
+}
+spl_autoload_register("My_autoload");*/
+
 $obj = new db();
 $obj->print();
