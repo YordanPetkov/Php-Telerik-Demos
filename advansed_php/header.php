0873c56da02f86408f67aebc9950d55374baf5f4
YordanPetkov <yordan_petkov180@abv.bg>
2019-04-29 23:15:47 +0300

    Added download file demo.

--- advansed_php/header.php
+++ advansed_php/header.php
@@ -1,3 +1,14 @@
 <?php
-header ("HTTP/1.1 404 Not Found");
+//header ("HTTP/1.1 404 Not Found");
 //header ("Location : https:/abv.bg");
+//$file = MP4DIR.$_GET['down'].".mp3";
+$file = "downloadFile.txt";
+if(!file_exists($file))
+    header("HTTP/1.0 404 Not Found", true, 404);
+else {
+    header("Content-Type: text/plain");
+    header("Content-Lenght: ").filesize($file);
+    header("Content-Disposition: attachment;" 
+    ."filename={$file}");
+    echo file_get_contents($file);
+}
