148b7dcc7484b3bd07baa6fb127e909c056759be
YordanPetkov <yordan_petkov180@abv.bg>
2019-04-29 15:00:07 +0300

    Connect to databas.

---
+++ MySQL/connectiong_demo.php
@@ -0,0 +1,15 @@
+<?php
+define("DATABASE_NAME", "hr");
+
+$dblink = mysqli_connect ("localhost", "exlogin", "123456");
+
+if($dblink === false) {
+    echo "Error connecting server! \n";
+}
+
+if(mysqli_select_db($dblink, DATABASE_NAME) === false) {
+    echo "Error connecting database .<br />";
+}
+else {
+    echo "Connecting to database successfully .<br />"; 
+}
