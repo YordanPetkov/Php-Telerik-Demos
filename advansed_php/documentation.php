a86063965568a5e5da6b93c83d7a76448ad53d38
YordanPetkov <yordan_petkov180@abv.bg>
2019-04-30 15:42:31 +0300

    Documentation demo.

---
+++ advansed_php/documentation.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @param string $name The name of user.
+ * @return int Always 1.
+ */
+function Foo($name){
+    return 1;
+}
+
+
+return Foo("Gosho");
--- advansed_php/exceptions.php
+++ advansed_php/exceptions.php
@@ -11,3 +11,20 @@ try {
 } catch (Exception $e) {
     echo "You cannot divide by zero!";
 }
+
+
+try {
+    if(!$_POST['name'])
+        throw new Exception('No name  supplied', 1001);
+    if(!$_POST['email'])
+        throw new Exception('No email supplied', 1002);
+} catch (Exception $e) {
+    $code = $e->getCode();
+    if($code > 1000  && $code < 1003) {
+        echo "Please fill in all the data";
+    }
+    else {
+        throw $e;
+    }
+}
+
