0b0d99a9980f01f9ebff499c9649e083daa0a07b
YordanPetkov <yordan_petkov180@abv.bg>
2019-05-01 14:58:42 +0300

    Added filter.

--- php_course_projects.php/homeworks/homeworks/monthly_costs/index.php
+++ php_course_projects.php/homeworks/homeworks/monthly_costs/index.php
@@ -2,13 +2,31 @@
 mb_internal_encoding('UTF-8');
 $pageTitle = "Списък";
 require "header.php";
+$filter = -1;
 
+if(array_key_exists("filter",$_POST)){
+    $filter = $_POST['filter'];
+}
 
 ?>
 <div>
     <a href="form.php">Добави разход</a>
+    <form method="POST">
+        <select name='filter'>
+            <?php $selected = ($filter == "Всичко") ? "selected='selected'" : ""; ?>
+            <option value="Всички" <?php echo $selected ?>>Всички</option>
+            <?php
+                foreach ($types as $key => $value){
+                    $selected = ($filter == $value) ? "selected='selected'" : "";
+                    echo '<option '.$selected.' value='.$value.'>'.$value.'</option>';
+                }
+            ?>
+        </select>
+        <input type="submit" value="Филтрирай" />
+    </form>
 </div>
 <table style="border:1px solid black">
+
     <thead>
         <tr>
             <th>Дата</th>
@@ -25,18 +43,22 @@ require "header.php";
                     $sum = 0;
                     $indexOfColumn = 0;
                     foreach ($result as $row){
-
+                    
                     echo "<tr>";
                         $columns = explode('!', $row);
-                        foreach ($columns as $data){
-                        if($indexOfColumn == 2)$sum += (float)$data;
-                        echo '<td>
-                                '.htmlspecialchars($data) .'
-                              </td>';
-                        if($indexOfColumn == count($columns)-1)$indexOfColumn = 0;
-                        else $indexOfColumn++;
-                    
-                         }
+
+                        if(trim($filter) != "Всичко" && trim($columns[3]) == trim($filter)){
+                            foreach ($columns as $data){
+                                if($indexOfColumn == 2)$sum += (float)$data;
+                                echo '<td>
+                                        '.htmlspecialchars($data) .'
+                                    </td>';
+                                if($indexOfColumn == count($columns)-1)$indexOfColumn = 0;
+                                else $indexOfColumn++;
+                        
+                            }
+                        }
+                        
                     echo "</tr>";
                     }
                     
