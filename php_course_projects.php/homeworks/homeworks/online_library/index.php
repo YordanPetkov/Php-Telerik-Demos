8c1c91d7d64568cdf50f54588394c6f4590d921b
YordanPetkov <yordan_petkov180@abv.bg>
2019-05-08 20:09:55 +0300

    Added list with authors and books.

--- php_course_projects.php/homeworks/homeworks/online_library/index.php
+++ php_course_projects.php/homeworks/homeworks/online_library/index.php
@@ -5,5 +5,20 @@ include './inc/header.php';
 <a href="authors.php">Автори</a>
 <a href="add_book.php">Нова книга</a>
 <?php
+    $sql = 'SELECT * FROM books as b 
+    INNER JOIN books_authors as ba
+    ON b.book_id = ba.book_id
+    INNER JOIN authors as a
+    ON a.author_id = ba.author_id';
+    $q = mysqli_query($db, $sql);
+
+    echo '<table border=1><tr><td>Книга</td><td>Автори</td></tr>';
+    while ($row = mysqli_fetch_assoc($q)) {
+        echo '<tr><td>'.$row['book_title'].'</td>
+        <td>'.$row['author_name'].'</td></tr>';
+    }
+    echo '</table>';
+?>
+<?php
 include './inc/footer.php';
 
