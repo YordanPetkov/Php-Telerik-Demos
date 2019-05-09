ea4ce6a2a07116e0f143cb732a38bcb183262efb
YordanPetkov <yordan_petkov180@abv.bg>
2019-05-09 19:24:26 +0300

    Added salt

--- php_course_projects.php/security/databaseSecurity.php
+++ php_course_projects.php/security/databaseSecurity.php
@@ -1,14 +1,4 @@
 <?php
 $pass = "qwerty";
-$pass2 = 'qwertY';
-echo md5($pass);
-echo '<br />';
-echo md5($pass2);
-echo '<br />';
-echo sha1($pass);
-echo '<br />';
-echo sha1($pass2);
-echo '<br />';
-echo md5('d8578edf8458ce06fbc5bb76a58c5ca4');
-echo '<br />';
-echo md5('897c8fde25c5cc5270cda61425eed3c8');
+$salt = 'bnaibicasbjn';
+echo md5($salt.$pass);
