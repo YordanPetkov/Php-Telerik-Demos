f7264f1e749f0824d9d937da53e823feede0dfbd
YordanPetkov <yordan_petkov180@abv.bg>
2019-05-18 12:28:58 +0300

    Useed __call

--- php_course_projects.php/oop/index.php
+++ php_course_projects.php/oop/index.php
@@ -11,13 +11,15 @@ $user2->age = 22;
 $user2->username = 'Petko';
 
 $user2->email = 'gosho@abv.bg';
-
+$user2->hello(2,3,1);
 echo $user2->email;
 class User {
 
     public $username;
     public $age;
     private $data = [];
+
+    
 
     public function __construct(){
         //echo 'Constructor' . PHP_EOL;
@@ -29,6 +31,11 @@ class User {
 
     public function __get($name) {
         return $this->data[$name];
+    }
+
+    public function __call($name, $arguments) {
+        echo $name;
+        echo print_r($arguments,true);
     }
 
     public function normalizeName($name) {
