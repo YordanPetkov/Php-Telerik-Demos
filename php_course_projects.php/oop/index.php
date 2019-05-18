46dc70c0e2f9b7a98393d9ef70f80cb9521fd6f9
YordanPetkov <yordan_petkov180@abv.bg>
2019-05-18 12:07:47 +0300

    Use __set

--- php_course_projects.php/oop/index.php
+++ php_course_projects.php/oop/index.php
@@ -3,14 +3,32 @@
 $user = new User();
 $user->username = "Gosho";
 $user->age = 18;
+echo $user->normalizeName('ivan');
+
 
 $user2 = new User();
 $user2->age = 22;
 $user2->username = 'Petko';
 
+$user2->email = 'gosho@abv.bg';
 class User {
 
     public $username;
     public $age;
 
+    public function __construct(){
+        echo 'Constructor' . PHP_EOL;
+    }
+
+    public function __set($name, $value) {
+        echo $name . '||' . $value . PHP_EOL;
+    }
+    public function normalizeName($name) {
+        $this->denormalizeName();
+        return strtoupper($name);
+    }
+
+    public function denormalizeName() {
+        echo 'exit' . PHP_EOL;
+    }
 }
