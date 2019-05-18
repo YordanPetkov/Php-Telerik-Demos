8e09666f9641b947e1bc3065cf0e0ff3fef5fdb9
YordanPetkov <yordan_petkov180@abv.bg>
2019-05-18 12:11:49 +0300

    Reg pattern

--- php_course_projects.php/oop/index.php
+++ php_course_projects.php/oop/index.php
@@ -3,7 +3,7 @@
 $user = new User();
 $user->username = "Gosho";
 $user->age = 18;
-echo $user->normalizeName('ivan');
+//echo $user->normalizeName('ivan');
 
 
 $user2 = new User();
@@ -11,24 +11,32 @@ $user2->age = 22;
 $user2->username = 'Petko';
 
 $user2->email = 'gosho@abv.bg';
+
+echo $user2->email;
 class User {
 
     public $username;
     public $age;
+    private $data = [];
 
     public function __construct(){
-        echo 'Constructor' . PHP_EOL;
+        //echo 'Constructor' . PHP_EOL;
     }
 
     public function __set($name, $value) {
-        echo $name . '||' . $value . PHP_EOL;
+        $this->data[$name] = $value;
     }
+
+    public function __get($name) {
+        return $this->data[$name];
+    }
+
     public function normalizeName($name) {
         $this->denormalizeName();
         return strtoupper($name);
     }
 
     public function denormalizeName() {
-        echo 'exit' . PHP_EOL;
+        //echo 'exit' . PHP_EOL;
     }
 }
