6ca3ddb9b660ab560886c5cee5fb08cae01a2fe1
YordanPetkov <yordan_petkov180@abv.bg>
2019-05-19 02:09:18 +0300

    Use static method

--- php_course_projects.php/oop/demo1.php
+++ php_course_projects.php/oop/demo1.php
@@ -3,10 +3,16 @@
 class User {
 
     private $username;
+    public static $numUsers;
 
+    public static function doSthStatic() {
+        echo 'Static';
+    }
     public function getUsername() {
         return $this->username;
     }
+
+
 
     public function setUsername($name) {
         $this->username = $name;
@@ -20,4 +26,8 @@ $ivan->setUsername('Ivan');
 $petko->setUsername('Petko');
 
 echo $ivan->getUsername() . PHP_EOL;
-echo $petko->getUsername();
+echo $petko->getUsername() . PHP_EOL;
+
+User::$numUsers = 1;
+User::doSthStatic();
+
