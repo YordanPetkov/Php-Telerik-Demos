f2257fb764c7353f4d097293d1fb89981052f695
YordanPetkov <yordan_petkov180@abv.bg>
2019-05-18 13:17:54 +0300

    Inheritance

---
+++ php_course_projects.php/oop/inh.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class Human extends Mammal{
+    public function playLoL() {
+        echo 'playing';
+    }
+
+    public function move() {
+        echo 'human move';
+    }
+}
+
+class Monkey extends Mammal {
+    public function throwbanana() {
+        echo "Throw <br />";
+    }
+}
+
+class Mammal {
+    public function move() {
+        echo 'move <br />';
+    }
+
+    public function eat () {
+        echo 'eat <br />';
+    }
+
+
+}
+
+$ivan = new Human() ;
+$ivan -> move();
