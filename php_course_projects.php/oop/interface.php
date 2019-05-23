2d12408d5efba805855e43e2729bab363bc10cc7
YordanPetkov <yordan_petkov180@abv.bg>
2019-05-23 18:38:50 +0300

    Use interfaces

---
+++ php_course_projects.php/oop/interface.php
@@ -0,0 +1,32 @@
+<?php
+
+interface IWriteInterface {
+    public function write($data);
+}
+
+class Article {
+    private $data;
+    
+    public function write($obj) {
+        $obj->write($this->data);
+    }
+}
+
+class FileWriter implements IWriteInterface{
+    
+    public function write($data) {
+        echo 'write to file';
+    }
+}
+
+class SendToSocket implements IWriteInterface{
+    
+    public function write($data) {
+        echo 'write to socket';
+    }
+}
+
+$a = new Article();
+$writer = new FileWriter();
+
+$a->write($writer);
