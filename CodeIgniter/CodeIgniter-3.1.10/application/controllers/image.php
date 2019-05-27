c04fb477c8ce87401a847c78584c22e760cfc3db
YordanPetkov <yordan_petkov180@abv.bg>
2019-05-27 11:28:51 +0300

    Added test controller

---
+++ CodeIgniter/CodeIgniter-3.1.10/application/controllers/image.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class image extends CI_Controller {
+
+	public function index() {
+        echo 'hello from test controler';
+    }
+
+    public function get() {
+        echo 'Hello';
+    }
+
+    
+}
