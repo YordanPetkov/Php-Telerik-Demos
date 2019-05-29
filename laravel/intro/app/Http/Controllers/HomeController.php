4318a46f30e9df0fff6e5874033c76273f10f3d0
YordanPetkov <yordan_petkov180@abv.bg>
2019-05-29 14:58:20 +0300

    Add blade demo.

--- laravel/intro/app/Http/Controllers/HomeController.php
+++ laravel/intro/app/Http/Controllers/HomeController.php
@@ -3,11 +3,15 @@
 namespace CoolBlog\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Request as IlluminateRequest;
 
 class HomeController extends Controller
 {
     
     public function index($id) {
-        return '#'.$id.' Hello from controller.';
+        /* var_dump(IlluminateRequest::get('sort'));
+        return '#'.$id.' Hello from controller.'; */
+
+        return view('public.demo');
     }
 }
